b4c3f7639dcb57f27527ff0b7e7d2038b6d8e3cc
Carlos Loureda <carloslouredaparrado@gmail.com>
2024-10-09 03:29:01 +0200

    some clean up

--- main.py
+++ main.py
@@ -1,36 +1,91 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-# URL of the page we want to scrape
-url = "http://www.agpool.com/index.php?ctx=competicion&submenu=competicioncoruna"
+def clean_text(text):
+    replacements = {
+        'Ã‘': 'Ñ',
+        'Âª': 'ª',
+        'Ã': 'Í',
+        'Í“': 'Ó',
+        'Âº': 'ª',
+    }
+    
+    for key, value in replacements.items():
+        text = text.replace(key, value)
+    
+    return text
 
-def scrape_temporadas(url):
-    #  Send a GET request to fetch the HTML content
-    response = requests.get(url)
+def get_final_segment(input_string):
+    # Split the string by hyphen and get the last segment
+    segments = input_string.split('-')
+    return segments[-1]
 
-    html_content = response.content
+def get_temps_info(url, resultados, competicion_key):
+    driver.get(url)
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(html_content, "html.parser")
+    # Espera a que el select 'anno' esté disponible
+    try:
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "anno")))
+    except TimeoutException:
+        print("El elemento 'anno' no se encontró a tiempo.")
+        driver.quit()
+        return
 
-    # Find the select element with name 'anno'
-    select_element = soup.find("select", {"name": "anno"})
+    select_temporada = Select(driver.find_element(By.ID, "anno"))
+    annos = [{'value': clean_text(option.get_attribute('value')), 'text': clean_text(option.text)} for option in select_temporada.options]
 
-    # Extract all options from the select element
-    options = select_element.find_all("option")
+    for anno in annos:
+        try:
+            select_temporada.select_by_value(anno['value'])
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "cambioano"))).click()
 
-    # Create a dictionary to store temporada ID and their names
-    temporadas = {}
+            time.sleep(2)  # Añadir una pequeña espera tras el click
 
-    for option in options:
-        temporada_id = option.get("value")
-        temporada_name = option.text.strip()
-        temporadas[temporada_id] = temporada_name
+            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "indice")))
 
-    # Print the extracted temporada options
-    print("Available Temporadas:")
-    for temporada_id, temporada_name in temporadas.items():
-        print(f"ID: {temporada_id}, Name: {temporada_name}")    
+            select_liga = Select(driver.find_element(By.ID, "indice"))
+            ligas = [{'value': clean_text(option.get_attribute('value')).replace('|', '-').replace(' ', '-').lower(), 'label': clean_text(option.text)} for option in select_liga.options]
+            
+            # Guardamos las ligas bajo la temporada dentro de la ciudad 'a-coruna'
+            temporada_key = get_final_segment(anno['text'].lower().replace(' ', '-'))
+            resultados[competicion_key][temporada_key] = ligas
+
+            driver.get(url)
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "anno")))
+            select_temporada = Select(driver.find_element(By.ID, "anno"))
+        except (NoSuchElementException, TimeoutException) as e:
+            print(f"Error al procesar el año {anno['text']}: {e}")
+            continue
+
+    # Escribimos el resultado en un archivo JSON
+    with open('resultados_ligas.json', 'w', encoding='utf-8') as json_file:
+        json.dump(resultados, json_file, ensure_ascii=False, indent=4)
 
 
-scrape_temporadas(url)       
+driver = webdriver.Chrome()
+def get_all_temps_info():
+    competiciones=['competicioncoruna','competicionlugo','competicionorense','competicionpontevedra']
+
+    resultados = {
+        "coruna": {},
+        "lugo": {},
+        "orense": {},
+        "pontevedra": {}
+    }
+    for competicion in competiciones:
+        url = f'http://www.agpool.com/index.php?ctx=competicion&submenu={competicion}'
+        competicion_key = competicion.replace('competicion', '').lower()
+        get_temps_info(url, resultados, competicion_key)
+    driver.quit()
+
+
+get_all_temps_info()
+# TODO: Use headless browser to scrape data in the server
+# TODO: Scrape data for tables and create a nice json file
+# TODO: Use the jsons in the website and put together both codes
